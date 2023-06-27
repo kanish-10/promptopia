b4fb970e02a4d3e1764108e47415f464791f9484
Kanish Chheda <kanishchheda@gmail.com>
2023-06-28 03:44:17 +0530

    First commit

---
+++ app/api/users/[id]/posts/route.js
@@ -0,0 +1,15 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const GET = async (request, { params }) => {
+  try {
+    await connectToDB();
+    console.log(params);
+    const prompts = await Prompt.find({ creator: params.id }).populate(
+      "creator"
+    );
+    return new Response(JSON.stringify(prompts), { status: 200 });
+  } catch (error) {
+    return new Response("Fail to fetch prompts", { status: 500 });
+  }
+};
